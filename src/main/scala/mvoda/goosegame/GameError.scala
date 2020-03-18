f3acc6ce7af4d1881b005efac781f2a54dfa03f1
mvoda <mircearaul.voda@gmail.com>
2020-03-18 17:59:36 +0100

    bring back game error

---
+++ src/main/scala/mvoda/goosegame/GameError.scala
@@ -0,0 +1,11 @@
+package mvoda.goosegame
+
+sealed trait GameError extends Throwable with GameMessage
+
+case class InvalidMove(first: Int, second: Int) extends GameError {
+  override val msg: String = s"invalid dice values: $first, $second"
+}
+
+case class InvalidCommand(string: String) extends GameError {
+  override val msg: String = s"invalid command: $string"
+}
