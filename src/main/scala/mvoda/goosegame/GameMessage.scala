fb794ab041a671781c3e946b13c376a42a29cf16
mvoda <mircearaul.voda@gmail.com>
2020-03-18 17:11:25 +0100

    add playerRolls message

--- src/main/scala/mvoda/goosegame/GameMessage.scala
+++ src/main/scala/mvoda/goosegame/GameMessage.scala
@@ -1,17 +1,21 @@
 package mvoda.goosegame
 
 trait GameMessage {
-  def message: String
+  def msg: String
 }
 
 case class PlayerAlreadyExists(player: Player) extends GameMessage {
-  override def message: String = s"$player: already existing player"
+  override val msg: String = s"$player: already existing player."
 }
 
 case class PlayerDoesNotExist(player: Player) extends GameMessage {
-  override def message: String = s"$player: does not exist"
+  override val msg: String = s"$player: does not exist."
 }
 
 case class ExistingPlayers(playerSet: Set[Player]) extends GameMessage {
-  override def message: String = s"players: ${playerSet.map(_.name).mkString(", ")}"
+  override val msg: String = s"players: ${playerSet.map(_.name).mkString(", ")}."
+}
+
+case class PlayerRolls(player: Player, firstDice: Int, secondDice: Int) extends GameMessage {
+  override val msg: String = s"$player rolls $firstDice, $secondDice."
 }
