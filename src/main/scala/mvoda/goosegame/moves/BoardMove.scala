1e7964024b9c76836f17dd215b60b873011c5f8f
mvoda <mircearaul.voda@gmail.com>
2020-03-18 17:13:20 +0100

    refactor board moves

--- src/main/scala/mvoda/goosegame/moves/BoardMove.scala
+++ src/main/scala/mvoda/goosegame/moves/BoardMove.scala
@@ -9,28 +9,28 @@ sealed trait BoardMove extends GameMessage {
 }
 
 case class Return(player: Player, start: BoardSpace, end: BoardSpace) extends BoardMove {
-  override def message: String = s"On $start there is $player, who returns to $end."
+  override def msg: String = s"On $start there is $player, who returns to $end."
 }
 
 case class Advance(player: Player, start: BoardSpace, end: BoardSpace) extends BoardMove {
-  override def message: String = s"$player moves from $start to $end."
+  override def msg: String = s"$player moves from $start to $end."
 }
 
 case class Overshot(player: Player, start: BoardSpace, end: BoardSpace, overshotSpaces: Int) extends BoardMove {
-  override def message: String = s"$player moves from $start to $end."
+  override def msg: String = s"$player moves from $start to $end."
 }
 
 case class Bounce(player: Player, start: BoardSpace, end: BoardSpace) extends BoardMove {
-  override def message: String = s"$player bounces! $player returns to $end."
+  override def msg: String = s"$player bounces! $player returns to $end."
 }
 
 case class Jump(player: Player, start: BoardSpace, end: BoardSpace) extends BoardMove {
-  override def message: String = s"$player jumps to $end."
+  override def msg: String = s"$player jumps to $end."
 }
 
 case class ExtraMove(move: BoardMove) extends BoardMove with GameMessage {
-  val player: Player           = move.player
-  val start: BoardSpace        = move.start
-  val end: BoardSpace          = move.end
-  override def message: String = s"$player moves again and goes to $end."
+  val player: Player       = move.player
+  val start: BoardSpace    = move.start
+  val end: BoardSpace      = move.end
+  override def msg: String = s"$player moves again and goes to $end."
 }
